f78bcd094d53e5d499a3a510df7768be0171ee01
Diego Adami Biancardi <diegoadamibiancardi@gmail.com>
2022-02-19 13:52:42 -0300

    Siglas dos estados adicionado ao JComboBox

--- src/cep/Cep.java
+++ src/cep/Cep.java
@@ -16,18 +16,19 @@ import java.awt.Cursor;
 import javax.swing.ImageIcon;
 import javax.swing.border.LineBorder;
 import java.awt.Color;
+import javax.swing.DefaultComboBoxModel;
 
 @SuppressWarnings("serial")
 public class Cep extends JFrame {
 
 	private JPanel contentPane;
-	private JTextField textField;
-	private JTextField textField_1;
-	private JTextField textField_2;
+	private JTextField txtCep;
+	private JTextField txtEndereco;
+	private JTextField txtBairro;
 	private JLabel lblNewLabel_3;
-	private JTextField textField_3;
+	private JTextField txtCidade;
 	private JLabel lblNewLabel_4;
-	private JButton btnNewButton_2;
+	private JButton btnSobre;
 
 	/**
 	 * Launch the application.
@@ -64,69 +65,70 @@ public class Cep extends JFrame {
 		lblNewLabel.setBounds(54, 76, 46, 14);
 		contentPane.add(lblNewLabel);
 		
-		textField = new JTextField();
-		textField.setBorder(new LineBorder(new Color(0, 0, 0)));
-		textField.setBounds(87, 70, 109, 20);
-		contentPane.add(textField);
-		textField.setColumns(10);
+		txtCep = new JTextField();
+		txtCep.setBorder(new LineBorder(new Color(0, 0, 0)));
+		txtCep.setBounds(87, 70, 109, 20);
+		contentPane.add(txtCep);
+		txtCep.setColumns(10);
 		
 		JLabel lblNewLabel_1 = new JLabel("Endere\u00E7o");
 		lblNewLabel_1.setBounds(23, 107, 54, 14);
 		contentPane.add(lblNewLabel_1);
 		
-		textField_1 = new JTextField();
-		textField_1.setBorder(new LineBorder(new Color(0, 0, 0)));
-		textField_1.setBounds(87, 104, 311, 20);
-		contentPane.add(textField_1);
-		textField_1.setColumns(10);
+		txtEndereco = new JTextField();
+		txtEndereco.setBorder(new LineBorder(new Color(0, 0, 0)));
+		txtEndereco.setBounds(87, 104, 311, 20);
+		contentPane.add(txtEndereco);
+		txtEndereco.setColumns(10);
 		
 		JLabel lblNewLabel_2 = new JLabel("Bairro");
 		lblNewLabel_2.setBounds(42, 140, 46, 14);
 		contentPane.add(lblNewLabel_2);
 		
-		textField_2 = new JTextField();
-		textField_2.setBorder(new LineBorder(new Color(0, 0, 0)));
-		textField_2.setBounds(87, 137, 311, 20);
-		contentPane.add(textField_2);
-		textField_2.setColumns(10);
+		txtBairro = new JTextField();
+		txtBairro.setBorder(new LineBorder(new Color(0, 0, 0)));
+		txtBairro.setBounds(87, 137, 311, 20);
+		contentPane.add(txtBairro);
+		txtBairro.setColumns(10);
 		
 		lblNewLabel_3 = new JLabel("Cidade");
 		lblNewLabel_3.setBounds(42, 171, 46, 14);
 		contentPane.add(lblNewLabel_3);
 		
-		textField_3 = new JTextField();
-		textField_3.setBorder(new LineBorder(new Color(0, 0, 0)));
-		textField_3.setBounds(87, 168, 216, 20);
-		contentPane.add(textField_3);
-		textField_3.setColumns(10);
+		txtCidade = new JTextField();
+		txtCidade.setBorder(new LineBorder(new Color(0, 0, 0)));
+		txtCidade.setBounds(87, 168, 216, 20);
+		contentPane.add(txtCidade);
+		txtCidade.setColumns(10);
 		
 		lblNewLabel_4 = new JLabel("UF");
 		lblNewLabel_4.setBounds(313, 171, 21, 14);
 		contentPane.add(lblNewLabel_4);
 		
-		JComboBox comboBox = new JComboBox();
-		comboBox.setBorder(null);
-		comboBox.setBackground(SystemColor.menu);
-		comboBox.setBounds(334, 168, 64, 22);
-		contentPane.add(comboBox);
+		JComboBox cboUf = new JComboBox();
+		cboUf.setModel(new DefaultComboBoxModel(new String[] {"", "AC", "AL", "AP", "AM", "BA", "CE", "DF", "ES", "GO", "MA", "MT", "MS", "MG", "PA", "PB", "PR", "PE", "PI", "RJ", "RN", "RS", "RO", "RR", "SC", "SP", "SE", "TO"}));
+		cboUf.setBorder(null);
+		cboUf.setBackground(SystemColor.menu);
+		cboUf.setBounds(334, 168, 64, 22);
+		contentPane.add(cboUf);
 		
-		JButton btnNewButton = new JButton("Limpar");
-		btnNewButton.setBackground(SystemColor.menu);
-		btnNewButton.setBounds(309, 69, 89, 23);
-		contentPane.add(btnNewButton);
+		JButton btnLimpar = new JButton("Limpar");
+		btnLimpar.setBackground(SystemColor.menu);
+		btnLimpar.setBounds(309, 69, 89, 23);
+		contentPane.add(btnLimpar);
 		
-		JButton btnNewButton_1 = new JButton("Buscar");
-		btnNewButton_1.setBackground(SystemColor.menu);
-		btnNewButton_1.setBounds(206, 69, 89, 23);
-		contentPane.add(btnNewButton_1);
+		JButton btnCep = new JButton("Buscar");
+		btnCep.setBackground(SystemColor.menu);
+		btnCep.setBounds(206, 69, 89, 23);
+		contentPane.add(btnCep);
 		
-		btnNewButton_2 = new JButton("");
-		btnNewButton_2.setBackground(SystemColor.control);
-		btnNewButton_2.setToolTipText("Sobre");
-		btnNewButton_2.setBorder(null);
-		btnNewButton_2.setIcon(new ImageIcon(Cep.class.getResource("/img/about.png")));
-		btnNewButton_2.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
-		btnNewButton_2.setBounds(376, 202, 48, 48);
-		contentPane.add(btnNewButton_2);
+		btnSobre = new JButton("");
+		btnSobre.setBackground(SystemColor.control);
+		btnSobre.setToolTipText("Sobre");
+		btnSobre.setBorder(null);
+		btnSobre.setIcon(new ImageIcon(Cep.class.getResource("/img/about.png")));
+		btnSobre.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+		btnSobre.setBounds(376, 202, 48, 48);
+		contentPane.add(btnSobre);
 	}
 }
