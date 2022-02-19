b64a192f9cf7a5bf89fc6c52fc5d4442ede675fc
Diego Adami Biancardi <diegoadamibiancardi@gmail.com>
2022-02-19 01:10:42 -0300

    Mais ajustes da interface principal - inplementação da interface 'Sobre'

--- src/cep/Cep.java
+++ src/cep/Cep.java
@@ -11,6 +11,11 @@ import javax.swing.JLabel;
 import javax.swing.JTextField;
 import javax.swing.JComboBox;
 import javax.swing.JButton;
+import java.awt.SystemColor;
+import java.awt.Cursor;
+import javax.swing.ImageIcon;
+import javax.swing.border.LineBorder;
+import java.awt.Color;
 
 @SuppressWarnings("serial")
 public class Cep extends JFrame {
@@ -22,6 +27,7 @@ public class Cep extends JFrame {
 	private JLabel lblNewLabel_3;
 	private JTextField textField_3;
 	private JLabel lblNewLabel_4;
+	private JButton btnNewButton_2;
 
 	/**
 	 * Launch the application.
@@ -49,60 +55,78 @@ public class Cep extends JFrame {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
+		contentPane.setCursor(Cursor.getPredefinedCursor(Cursor.DEFAULT_CURSOR));
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
 		JLabel lblNewLabel = new JLabel("CEP:");
-		lblNewLabel.setBounds(42, 76, 46, 14);
+		lblNewLabel.setBounds(54, 76, 46, 14);
 		contentPane.add(lblNewLabel);
 		
 		textField = new JTextField();
-		textField.setBounds(75, 70, 109, 20);
+		textField.setBorder(new LineBorder(new Color(0, 0, 0)));
+		textField.setBounds(87, 70, 109, 20);
 		contentPane.add(textField);
 		textField.setColumns(10);
 		
 		JLabel lblNewLabel_1 = new JLabel("Endere\u00E7o");
-		lblNewLabel_1.setBounds(19, 107, 46, 14);
+		lblNewLabel_1.setBounds(23, 107, 54, 14);
 		contentPane.add(lblNewLabel_1);
 		
 		textField_1 = new JTextField();
-		textField_1.setBounds(75, 104, 349, 20);
+		textField_1.setBorder(new LineBorder(new Color(0, 0, 0)));
+		textField_1.setBounds(87, 104, 311, 20);
 		contentPane.add(textField_1);
 		textField_1.setColumns(10);
 		
 		JLabel lblNewLabel_2 = new JLabel("Bairro");
-		lblNewLabel_2.setBounds(30, 140, 46, 14);
+		lblNewLabel_2.setBounds(42, 140, 46, 14);
 		contentPane.add(lblNewLabel_2);
 		
 		textField_2 = new JTextField();
-		textField_2.setBounds(75, 137, 349, 20);
+		textField_2.setBorder(new LineBorder(new Color(0, 0, 0)));
+		textField_2.setBounds(87, 137, 311, 20);
 		contentPane.add(textField_2);
 		textField_2.setColumns(10);
 		
 		lblNewLabel_3 = new JLabel("Cidade");
-		lblNewLabel_3.setBounds(30, 171, 46, 14);
+		lblNewLabel_3.setBounds(42, 171, 46, 14);
 		contentPane.add(lblNewLabel_3);
 		
 		textField_3 = new JTextField();
-		textField_3.setBounds(75, 168, 216, 20);
+		textField_3.setBorder(new LineBorder(new Color(0, 0, 0)));
+		textField_3.setBounds(87, 168, 216, 20);
 		contentPane.add(textField_3);
 		textField_3.setColumns(10);
 		
 		lblNewLabel_4 = new JLabel("UF");
-		lblNewLabel_4.setBounds(301, 171, 46, 14);
+		lblNewLabel_4.setBounds(313, 171, 21, 14);
 		contentPane.add(lblNewLabel_4);
 		
 		JComboBox comboBox = new JComboBox();
-		comboBox.setBounds(322, 168, 102, 22);
+		comboBox.setBorder(null);
+		comboBox.setBackground(SystemColor.menu);
+		comboBox.setBounds(334, 168, 64, 22);
 		contentPane.add(comboBox);
 		
 		JButton btnNewButton = new JButton("Limpar");
-		btnNewButton.setBounds(19, 227, 89, 23);
+		btnNewButton.setBackground(SystemColor.menu);
+		btnNewButton.setBounds(309, 69, 89, 23);
 		contentPane.add(btnNewButton);
 		
 		JButton btnNewButton_1 = new JButton("Buscar");
-		btnNewButton_1.setBounds(194, 69, 89, 23);
+		btnNewButton_1.setBackground(SystemColor.menu);
+		btnNewButton_1.setBounds(206, 69, 89, 23);
 		contentPane.add(btnNewButton_1);
+		
+		btnNewButton_2 = new JButton("");
+		btnNewButton_2.setBackground(SystemColor.control);
+		btnNewButton_2.setToolTipText("Sobre");
+		btnNewButton_2.setBorder(null);
+		btnNewButton_2.setIcon(new ImageIcon(Cep.class.getResource("/img/about.png")));
+		btnNewButton_2.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+		btnNewButton_2.setBounds(376, 202, 48, 48);
+		contentPane.add(btnNewButton_2);
 	}
 }
