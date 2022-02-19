6e6575125c4069d4624baa054b866676b3fd8e30
Diego Adami Biancardi <diegoadamibiancardi@gmail.com>
2022-02-19 15:37:13 -0300

    Teste do método buscarCep - Bairro, Cidade e UF ok

--- src/cep/Cep.java
+++ src/cep/Cep.java
@@ -39,6 +39,7 @@ public class Cep extends JFrame {
 	private JTextField txtCidade;
 	private JLabel lblNewLabel_4;
 	private JButton btnSobre;
+	private JComboBox cboUf;
 
 	/**
 	 * Launch the application.
@@ -70,71 +71,72 @@ public class Cep extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
+
 		JLabel lblNewLabel = new JLabel("CEP:");
 		lblNewLabel.setBounds(54, 76, 46, 14);
 		contentPane.add(lblNewLabel);
-		
+
 		txtCep = new JTextField();
 		txtCep.setBorder(new LineBorder(new Color(0, 0, 0)));
 		txtCep.setBounds(87, 70, 109, 20);
 		contentPane.add(txtCep);
 		txtCep.setColumns(10);
-		
+
 		JLabel lblNewLabel_1 = new JLabel("Endere\u00E7o");
 		lblNewLabel_1.setBounds(23, 107, 54, 14);
 		contentPane.add(lblNewLabel_1);
-		
+
 		txtEndereco = new JTextField();
 		txtEndereco.setBorder(new LineBorder(new Color(0, 0, 0)));
 		txtEndereco.setBounds(87, 104, 311, 20);
 		contentPane.add(txtEndereco);
 		txtEndereco.setColumns(10);
-		
+
 		JLabel lblNewLabel_2 = new JLabel("Bairro");
 		lblNewLabel_2.setBounds(42, 140, 46, 14);
 		contentPane.add(lblNewLabel_2);
-		
+
 		txtBairro = new JTextField();
 		txtBairro.setBorder(new LineBorder(new Color(0, 0, 0)));
 		txtBairro.setBounds(87, 137, 311, 20);
 		contentPane.add(txtBairro);
 		txtBairro.setColumns(10);
-		
+
 		lblNewLabel_3 = new JLabel("Cidade");
 		lblNewLabel_3.setBounds(42, 171, 46, 14);
 		contentPane.add(lblNewLabel_3);
-		
+
 		txtCidade = new JTextField();
 		txtCidade.setBorder(new LineBorder(new Color(0, 0, 0)));
 		txtCidade.setBounds(87, 168, 216, 20);
 		contentPane.add(txtCidade);
 		txtCidade.setColumns(10);
-		
+
 		lblNewLabel_4 = new JLabel("UF");
 		lblNewLabel_4.setBounds(313, 171, 21, 14);
 		contentPane.add(lblNewLabel_4);
-		
-		JComboBox cboUf = new JComboBox();
-		cboUf.setModel(new DefaultComboBoxModel(new String[] {"", "AC", "AL", "AP", "AM", "BA", "CE", "DF", "ES", "GO", "MA", "MT", "MS", "MG", "PA", "PB", "PR", "PE", "PI", "RJ", "RN", "RS", "RO", "RR", "SC", "SP", "SE", "TO"}));
+
+		cboUf = new JComboBox();
+		cboUf.setModel(new DefaultComboBoxModel(
+				new String[] { "", "AC", "AL", "AP", "AM", "BA", "CE", "DF", "ES", "GO", "MA", "MT", "MS", "MG", "PA",
+						"PB", "PR", "PE", "PI", "RJ", "RN", "RS", "RO", "RR", "SC", "SP", "SE", "TO" }));
 		cboUf.setBorder(null);
 		cboUf.setBackground(SystemColor.menu);
 		cboUf.setBounds(334, 168, 64, 22);
 		contentPane.add(cboUf);
-		
+
 		JButton btnLimpar = new JButton("Limpar");
 		btnLimpar.setBackground(SystemColor.menu);
 		btnLimpar.setBounds(309, 69, 89, 23);
 		contentPane.add(btnLimpar);
-		
+
 		JButton btnCep = new JButton("Buscar");
 		btnCep.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				if(txtCep.getText().equals("")) {
+				if (txtCep.getText().equals("")) {
 					JOptionPane.showMessageDialog(null, "É preciso informar o CEP");
-					txtCep.requestFocus(); 
-				}
-				else {
+					txtCep.requestFocus();
+				} else {
 					buscarCep();
 				}
 			}
@@ -142,7 +144,7 @@ public class Cep extends JFrame {
 		btnCep.setBackground(SystemColor.menu);
 		btnCep.setBounds(206, 69, 89, 23);
 		contentPane.add(btnCep);
-		
+
 		btnSobre = new JButton("");
 		btnSobre.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -157,83 +159,39 @@ public class Cep extends JFrame {
 		btnSobre.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
 		btnSobre.setBounds(376, 202, 48, 48);
 		contentPane.add(btnSobre);
-		
+
 		RestrictedTextField validar = new RestrictedTextField(txtCep);
 		validar.setOnlyNums(true);
 		validar.setLimit(8);
 	}
-	
+
 	private void buscarCep() {
 		String logradouro = " ";
 		String tipoLogradouro = " ";
 		String resultado = null;
 		String cep = txtCep.getText();
-		
+
 		try {
 			URL url = new URL("http://cep.republicavirtual.com.br/web_cep.php?cep=" + cep + "&formato=xml");
 			SAXReader xml = new SAXReader();
 			Document documento = xml.read(url);
 			Element root = documento.getRootElement();
-			for(Iterator<Element> it = root.elementIterator(); it.hasNext();) {
+			for (Iterator<Element> it = root.elementIterator(); it.hasNext();) {
 				Element element = it.next();
-				if(element.getQualifiedName().equals("cidade")) {
+				if (element.getQualifiedName().equals("cidade")) {
 					txtCidade.setText(element.getText());
 				}
+				if (element.getQualifiedName().equals("bairro")) {
+					txtBairro.setText(element.getText());
+				}
+				if(element.getQualifiedName().equals("uf")) {
+					cboUf.setSelectedItem(element.getText());
+				}
 			}
-		}
-		catch(Exception e) {
+		} catch (Exception e) {
 			System.out.println(e);
 		}
-		
+
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+
 }
