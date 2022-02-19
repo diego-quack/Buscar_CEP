42d3704b0f79911fe56204524a8d6ba8a4b7fd3d
Diego Adami Biancardi <diegoadamibiancardi@gmail.com>
2022-02-19 01:25:02 -0300

    Mais ajustes de interface.

--- src/cep/Sobre.java
+++ src/cep/Sobre.java
@@ -4,6 +4,9 @@ import java.awt.EventQueue;
 
 import javax.swing.JDialog;
 import java.awt.Toolkit;
+import javax.swing.JLabel;
+import javax.swing.ImageIcon;
+import java.awt.Cursor;
 
 public class Sobre extends JDialog {
 
@@ -32,6 +35,22 @@ public class Sobre extends JDialog {
 		setTitle("Sobre");
 		setIconImage(Toolkit.getDefaultToolkit().getImage(Sobre.class.getResource("/img/home.png")));
 		setBounds(100, 100, 450, 300);
+		getContentPane().setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Buscar CEP - Ver 1");
+		lblNewLabel.setBounds(48, 108, 155, 14);
+		getContentPane().add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("Autor: Diego Adami Biancardi");
+		lblNewLabel_1.setBounds(48, 133, 208, 14);
+		getContentPane().add(lblNewLabel_1);
+		
+		JLabel lblNewLabel_2 = new JLabel("");
+		lblNewLabel_2.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+		lblNewLabel_2.setToolTipText("Reposit\u00F3rio do projeto");
+		lblNewLabel_2.setIcon(new ImageIcon(Sobre.class.getResource("/img/github (2).png")));
+		lblNewLabel_2.setBounds(281, 99, 48, 48);
+		getContentPane().add(lblNewLabel_2);
 
 	}
 
